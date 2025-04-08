9602c65f8b211b638d541904ab59ed684125b879
Lola Anggita <suchcemeann@gmail.com>
2025-04-08 23:02:10 +0700

    Tugas Seleksi SoftDev

--- Tugas Seleksi/soal6.js
+++ Tugas Seleksi/soal6.js
@@ -2,7 +2,7 @@ const menu = {
     "Ayam Goreng Krispi": { type: "Makanan", price: 15000 },
     "Ayam Puk Puk": { type: "Makanan", price: 13000 },
     "Ayam Bakar": { type: "Makanan", price: 20000 },
-    "Es teh": { type: "Minuman", price: 5000 },
+    "Es Teh": { type: "Minuman", price: 5000 },
     "Es Jeruk": { type: "Minuman", price: 7000 },
 };
 
@@ -16,5 +16,16 @@ function calculateBill(order) {
         return subtotal * 1.15; // Add 15% transaction tax
 }
 
-const rehanOrder = { "Ayam Bakar": 2, "Es teh": 1 };
-console.log(calculateBill(rehanOrder).toFixed(2));
+const rehanOrder = { "Ayam Bakar": 2, "Es Teh": 1 };
+const AmbaOrder = { "Ayam Puk Puk": 2, "Es Teh": 3 };
+const FaizOrder = {
+    "Ayam Goreng Krispi": 1,
+    "Ayam Puk Puk": 1,
+    "Ayam Bakar": 1,
+    "Es Teh": 1,
+    "Es Jeruk": 1
+};
+
+console.log("Total Harga Pesanan Rehan Whangsap: Rp" + calculateBill(rehanOrder).toFixed(2));
+console.log("Total Harga Pesanan Amba Roni: Rp" + calculateBill(AmbaOrder).toFixed(2));
+console.log("Total Harga Pesanan Faiz Ngawi: Rp" + calculateBill(FaizOrder).toFixed(2));
