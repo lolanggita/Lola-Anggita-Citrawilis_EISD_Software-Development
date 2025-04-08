60249e8ba8ecf47047612ed1f07bd9f64d125e1c
Lola Anggita <suchcemeann@gmail.com>
2025-04-08 23:04:39 +0700

    Tugas Seleksi SoftDev

--- Tugas Seleksi/soal7.js
+++ Tugas Seleksi/soal7.js
@@ -10,3 +10,7 @@ function decryptChat(encrypted) {
 }
 
 console.log(decryptChat("xfqfr bfmdz"));
+console.log(decryptChat("gjxtp lzj rfz ifkyfw jxi snm"));
+console.log(decryptChat("gwt, gjxtp qz rfz rfpfs in bfwlty lfp?"));
+console.log(decryptChat("fpz xfdfsl pfrz, rfz lfp ofin ufhfwpz"));
+console.log(decryptChat("dfsl pnwnr xynhpjw otrtp pz pnhp ifwn lwzu"));
